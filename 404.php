cf4fd93a9fc9c1f7e7af6f972a4c36ade47d105c
cesine <cesine@yahoo.com>
2014-07-17 12:52:07 -0400

    commenting out links to archives in the 404 page

--- 404.php
+++ 404.php
@@ -16,10 +16,10 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'pepbiz' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'pepbiz' ); ?></p>
 
 					<?php get_search_form(); ?>
-
+					<!--
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
 					<?php if ( pepbiz_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
@@ -44,12 +44,13 @@ get_header(); ?>
 					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'pepbiz' ), convert_smilies( ':)' ) ) . '</p>';
 					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 					?>
+ 
 
 					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
+					-->
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
-
+	        <p>&nbsp;</p>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
